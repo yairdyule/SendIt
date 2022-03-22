b6c869eea6e16283389e6f6ad31bcab10d9d73d6
Jared Jewell <jaje9434@colorado.edu>
2022-03-22 09:40:39 -0600

    update index.tsx
    Signed-off-by: Jared Jewell <jaje9434@colorado.edu>

--- supabase-nextts/pages/index.tsx
+++ supabase-nextts/pages/index.tsx
@@ -4,18 +4,25 @@ import { useEffect, useState } from "react";
 import Account from "../components/Account";
 import Auth from "../components/Auth";
 import { supabase } from "../utils/supabaseClient";
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+
+export type Profile = any;
 
 const Home: NextPage = () => {
-  const [session, setSession] = useState<Session | null>(null);
+  const [session, setSession] = useState<AuthSession | null>(null);
+  const [profiles, setProfiles] = useState<Profile[] | null>(null);
 
   useEffect(() => {
     setSession(supabase.auth.session());
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
+
+    supabase.auth.onAuthStateChange(
+      (_event: string, session: AuthSession | null) => {
+        setSession(session);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    getPublicProfiles();
   }, []);
 
   return (
@@ -23,10 +30,38 @@ const Home: NextPage = () => {
       {!session ? (
         <Auth />
       ) : (
-        <Account key={session?.user?.id} session={session} />
+        <div className="row">
+          <div className="col-6">
+            <h3>Account</h3>
+            <Account key={session?.user?.id} session={session} />
+          </div>
+          <div className="col-6">
+            <h3>Public Profiles</h3>
+            <ProfileList profiles={profiles} />
+          </div>
+        </div>
       )}
+
+      <Footer />
     </div>
   );
+
+  type Error = any;
+  async function getPublicProfiles() {
+    try {
+      const { data, error } = await supabase
+        .from<Profile>("profiles")
+        .select("id, username, avatar_url, website, updated_at")
+        .order("updated_at", { ascending: false });
+
+      if (!data || error) throw error || new Error("No data");
+
+      console.log("public profiles:", data);
+      setProfiles(data);
+    } catch (error: Error) {
+      console.log("error", error.message);
+    }
+  }
 };
 
 export default Home;
